4cbee1c899f64c826d5418191c5e53a5e7436a33
raquel <rachel050479@gmail.com>
2023-10-24 17:09:04 +0200

    added scss

--- src/components/proyecto/CardPreview.jsx
+++ src/components/proyecto/CardPreview.jsx
@@ -3,44 +3,45 @@ import PropTypes from 'prop-types';
 
 const CardPreview = ({ data }) => {
   return (
-    <section className="preview">
+    <section className='preview'>
       <img
-        className="image"
+        className='image'
         src={data.image || '../../images/playa.jpg'}
-        alt=""
+        alt=''
       />
 
-      <section className="autor">
-        <section className="info-project">
-          <p className="subtitle">Personal Project Card</p>
-          <hr className="line" />
+      <section className='autor'>
+        <section className='info-project'>
+          <p className='subtitle'>Personal Project Card</p>
+          <hr className='line' />
 
-          <h2 className="title">{data.name || 'Elegant Workspace'}</h2>
-          <p className="slogan">{data.slogan || 'Diseños Exclusivos'}</p>
-          <p className="desc">
+          <h2 className='title'>{data.name || 'Elegant Workspace'}</h2>
+          <p className='slogan'>{data.slogan || 'Diseños Exclusivos'}</p>
+          <p className='desc'>
             {data.desc ||
               'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Libero, delectus'}
           </p>
-          <section className="technologies">
-            <p className="text">{data.technologies || 'React JS, MongoDB'}</p>
+          <section className='technologies'>
+            <p className='text'>{data.technologies || 'React JS, MongoDB'}</p>
           </section>
-          <a href={data.demo} target="_blank">
-            <i className="fa-solid fa-globe"></i>
+          <div className='icon-div'>
+          <a className='icon-preview' href={data.demo} target='_blank'>
+            <i className='fa-solid fa-globe'></i>
           </a>
-
-          <a href={data.repo} target="_blank">
-            <i className="fa-brands fa-github"></i>
+          <a className='icon-preview' href={data.repo} target='_blank'>
+            <i className='fa-brands fa-github'></i>
           </a>
+          </div>
         </section>
 
-        <section className="info-autor">
+        <section className='info-autor'>
           <img
-            className="image"
+            className='image'
             src={data.photo || '../../images/hierbas.webp'}
-            alt=""
+            alt=''
           />
-          <p className="job">{data.job || 'Full Stack Developer'}</p>
-          <p className="name">{data.autor || 'Emmelie Björklund'}</p>
+          <p className='job'>{data.job || 'Full Stack Developer'}</p>
+          <p className='name'>{data.autor || 'Emmelie Björklund'}</p>
         </section>
       </section>
     </section>
