4c5772d6b32e525780a2bde3045caff723f47e8d
Silvia <silviatc1993@gmail.com>
2023-11-03 21:14:23 +0100

    added toggle menu in header

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,20 +1,50 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.scss';
 
 const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className='header'>
-      <i className='header__icon fa-brands fa-pagelines'></i>
-      <h1 className='header__h1'>Proyectos Molones</h1>
-      <i className='header__icon fa-solid fa-leaf'></i>
-      <p className='header__text'>
+    <header className="header">
+      <i className="header__icon fa-brands fa-pagelines"></i>
+      <h1 className="header__h1">Proyectos Molones</h1>
+      <i className="header__icon fa-solid fa-leaf"></i>
+      <p className="header__text">
         Escaparate en línea para recoger ideas a través de la tecnología.
       </p>
-      {/* <button className='header__btn'>Ver proyectos</button> */}
-      {/* <Link className='header__btn' to='/form'
-        >
-      Ver proyectos
-      </Link> */}
+      <section className="navigation__btns">
+        <button className="menu-toggle" onClick={toggleMenu}>
+          <i className="fa-solid fa-bars"></i>
+        </button>
+        {isMenuOpen ? (
+          <div className="dropdown-menu">
+            <Link className="navigation__btns-one" to="/" onClick={toggleMenu}>
+              Volver al inicio
+            </Link>
+            <Link
+              className="navigation__btns-two"
+              to="/projects"
+              onClick={toggleMenu}
+            >
+              Ver proyectos
+            </Link>
+            <Link
+              className="navigation__btns-three"
+              to="/form"
+              onClick={toggleMenu}
+            >
+              Crea tu proyecto
+            </Link>
+          </div>
+        ) : null}
+      </section>
     </header>
   );
 };
+
 export default Header;
